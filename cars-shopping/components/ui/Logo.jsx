74131f96b316428aaa0913c0053461195955332c
gulnuresgerova <akulaalekb@gmail.com>
2024-02-08 17:05:54 -0800

    eror var idi onu duzeltdim

--- cars-shopping/components/ui/Logo.jsx
+++ cars-shopping/components/ui/Logo.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
+
 const Logo = () => {
-  return(
-    <Link href="/">
-    <div className="text-[2rem] font-dancing font-bold">
-      <Image src="/images/logo-w.png" alt="" width={128} height={128} />
-    </div>
-  </Link>;
-  )
+  return <div className="text-[2rem] font-dancing font-bold">
+
+ <Image
+                    src="/images/logo-w.png"
+                    alt=""
+                    
+                    width={128}
+                    height={128}
+                  />
+  </div>;
 };
 
 export default Logo;
